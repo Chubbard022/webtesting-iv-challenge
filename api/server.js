d7b81d076ddd3f14268139258b88d6f353a4eb9a
Chubbard022 <chubbard022@gmail.com>
2019-05-16 13:04:26 -0700

    server up and running

--- api/server.js
+++ api/server.js
@@ -8,7 +8,7 @@ const server = express()
 server.use(express.json())
 server.use(helmet())
 server.use(cors())
-server.use("/api/hobbits",hobbitRouter)
+//server.use("/api/hobbits",hobbitRouter)
 
 server.get("/",(req,res)=>{
     res.status(200).json({api: "up"})
