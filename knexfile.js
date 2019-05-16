0129fe08b819790762823aba81bd14c3c8ae1889
Chubbard022 <chubbard022@gmail.com>
2019-05-16 12:52:53 -0700

    knex file up

---
+++ knexfile.js
@@ -0,0 +1,36 @@
+const localPg = {
+  host: "localhost",
+  database: "hobbits",
+  user: "student",
+  password: "hired"
+};
+const productionDbConnection = process.env.DATABASE_URL || localPg;
+
+module.exports = {
+  development: {
+    client: "sqlite3",
+    connection: {
+      filename: "./data/hobbits.db3"
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: "./data/migrations"
+    },
+    seeds: {
+      directory: "./data/seeds"
+    }
+  },
+  testing: {
+    client: "sqlite3",
+    connection: {
+      filename: "./data/test.db3"
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: "./data/migrations"
+    },
+    seeds: {
+      directory: "./data/seeds"
+    }
+  }
+}
